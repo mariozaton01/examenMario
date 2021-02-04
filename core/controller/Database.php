833100e3378799bea19cd584972153ca4f7d4fdf
mariozaton01 <2GDAW12@ARRIAGAINFO.LOCAL>
2021-02-04 08:55:02 +0100

    archivo

---
+++ core/controller/Database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+	public static $db;
+	public static $con;
+	function Database(){
+		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		//Copiar también la información de la conexión en la función de abajo connect()
+	}
+
+	function connect(){
+		//Copiar aquí también la info de la conexión
+		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
+		$con->query("set sql_mode=''");
+		return $con;
+	}
+
+	public static function getCon(){
+		if(self::$con==null && self::$db==null){
+			self::$db = new Database();
+			self::$con = self::$db->connect();
+		}
+		return self::$con;
+	}
+	
+}
+?>
