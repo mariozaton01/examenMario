22d884973934d21685e3968a849502e81a285b6e
mariozaton01 <2GDAW12@ARRIAGAINFO.LOCAL>
2021-02-04 09:03:25 +0100

    2archivo

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,13 +3,13 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="b8e31e28b5ff19";$this->pass="935fefcf";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_b54568f34421896";
 		//Copiar también la información de la conexión en la función de abajo connect()
 	}
 
 	function connect(){
 		//Copiar aquí también la info de la conexión
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="b8e31e28b5ff19";$this->pass="935fefcf";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_b54568f34421896";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
